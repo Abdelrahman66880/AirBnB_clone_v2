cecec10e4755dac95bed688b01cfd583bf462be9
Abdelrahman <abdofathi317@gmail.com>
2024-04-20 19:35:29 +0000

    Web Framework

--- models/state.py
+++ models/state.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
-import uuid
+""" holds class State"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
+class State(BaseModel, Base):
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
     def __init__(self, *args, **kwargs):
-        """Initialization method for State"""
+        """initializes state"""
         super().__init__(*args, **kwargs)
-        self.id = str(uuid.uuid4())  # Initialize id attribute
 
-    name = ""
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
